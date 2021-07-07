d7343ed16ac7d6e6a2cb746abe89ad1df40af417
Agon Mirashi <agon.mirashi@seatbelt.xyz>
2021-07-07 14:29:54 +0200

    - inherit class when new list added from previous list - fix on focus bug - fix on change font size bug

--- packages/ckeditor5-build-inline-creacheck/src/FontSize.js
+++ packages/ckeditor5-build-inline-creacheck/src/FontSize.js
@@ -54,39 +54,35 @@ class FontSize extends Plugin {
 	}
 
 	customFontSizeDropDown(editor) {
-		const { t } = editor.locale;
 		editor.ui.componentFactory.add('fontSizeDropdown', () => {
 			const editor = this.editor;
 
-			const command = editor.commands.get('fontSize');
-
 			// Use original fontSize button - we only changes its behavior.
 			const dropdownView = editor.ui.componentFactory.create('fontSize');
-
-			dropdownView.buttonView
-				.bind('label')
-				.to(command, 'value', (value) => {
-					if (
-						editor.editing.view.document.selection.focus &&
-						value !== undefined
-					) {
-						const listItem =
-							editor.editing.view.document.selection.focus.parent;
-						if (listItem.name == 'li') {
-							editor.editing.view.change((writer) => {
-								writer.setAttribute(
-									'class',
-									`text-${value}`,
-									listItem
-								);
-							});
-						}
-					}
-					// If no value is set on the command show 'Default' text.
-					// Use t() method to make that string translatable.
-					return value ? value : t('default');
-				});
-
+			dropdownView.on('execute', (evt) => {
+				const listItem =
+					editor.editing.view.document.selection.focus.parent;
+				if (listItem.name == 'li') {
+					editor.editing.view.change((writer) => {
+						writer.setAttribute(
+							'class',
+							`text-${evt.source.commandParam}`,
+							listItem
+						);
+					});
+				} else {
+					const listItem =
+						editor.editing.view.document.selection.focus.parent
+							.parent;
+					editor.editing.view.change((writer) => {
+						writer.setAttribute(
+							'class',
+							`text-${evt.source.commandParam}`,
+							listItem
+						);
+					});
+				}
+			});
 			return dropdownView;
 		});
 	}
