5b0f4de13023210ddb0255a2d5521f013e7b91f5
Andreas Elstner <andreas.elstner@orcaya.com>
2022-10-10 19:36:49 +0200

    fix fontsize bug

--- packages/ckeditor5-build-inline-creacheck/src/KeepFontsize.js
+++ packages/ckeditor5-build-inline-creacheck/src/KeepFontsize.js
@@ -4,11 +4,26 @@ class KeepFontsize extends Plugin {
 	init() {
 		const editor = this.editor;
 		this.ccListenForEvent( editor );
+		this.ccListenForEventParagraph( editor );
 	}
 
 	ccListenForEvent( editor ) {
 		editor.conversion.for( 'downcast' ).add( dispatcher => {
 			dispatcher.on( 'remove:$text', ( evt, data, conversionApi ) => {
+				if ( data.position.nodeBefore === null ) {
+					editor.execute( 'fontSize', { value: 'default' } );
+				}
+			} );
+		} );
+	}
+
+	/* This function is necessary if several lines e.g. can be marked
+	and deleted by ctrl + a. ccListenForEvent only reacts when text
+	within the p tag is deleted. ccListenForEventParagraph also reacts
+	across multiple lines or p tags */
+	ccListenForEventParagraph( editor ) {
+		editor.conversion.for( 'downcast' ).add( dispatcher => {
+			dispatcher.on( 'remove:paragraph', ( evt, data, conversionApi ) => {
 				if ( data.position.nodeBefore === null ) {
 					editor.execute( 'fontSize', { value: 'default' } );
 				}
